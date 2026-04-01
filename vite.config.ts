d15f73b2b2149b20f4f66c8e52b8a9bc9f8a22cf
aikeenk8 <aikeenkate04@gmail.com>
2026-04-01 16:11:56 +0800

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
-import * as path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png'],
+      manifest: {
+        name: 'compo',
+        short_name: 'compo',
+        description: 'a place to compose.',
+        theme_color: '#121214',
+        background_color: '#121214',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'public/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'public/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
